1bcd7f58f0e21b00c736bb2f92e73c81d57f1f80
Robert Diamond <robert.diamond@gmail.com>
2012-01-25 15:53:20 -0500

    update initial comments

--- ardwatch/ardwatch.ino
+++ ardwatch/ardwatch.ino
@@ -1,32 +1,5 @@
-                  /*
-BluetoothFrame Demo Code
-2011 Copyright (c) Seeed Technology Inc.  All right reserved.
- 
-Author: Visweswara R
- 
-This demo code is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
- 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
- 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- 
-For more details about the product please check http://www.seeedstudio.com/depot/
- 
-*/
- 
- 
-/* 1.Upload this sketch to Seeeduino Film
-   2.Power Off Seeeduino Film
-   3.Connect Bluetooth Frame
-   4.Connect Battery
+/*
+Arduino-based watch!
 */
 #include <avr/wdt.h>
 #include <MeetAndroid.h>
